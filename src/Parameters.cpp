1fdfb4e2887458a0d0d307b59ef2a1e554036255
Matthijs Hollemans <mail@hollance.com>
2024-07-12 18:10:45 +0200

    change default value for drive

--- src/Parameters.cpp
+++ src/Parameters.cpp
@@ -34,7 +34,7 @@ juce::AudioProcessorValueTreeState::ParameterLayout Parameters::createParameterL
         ParameterID::drive,
         "Drive",
         juce::NormalisableRange<float>(0.0f, 100.0f, 0.01f, 1.0f),
-        0.0f,
+        25.0f,
         juce::AudioParameterFloatAttributes().withLabel("%")));
 
     layout.add(std::make_unique<juce::AudioParameterChoice>(
